59d1ed74763a11c8f8f97f616b137513a01d2155
Paul Eastham <eastham@gmail.com>
2025-04-18 19:06:48 -0700

    disable manual exp control

--- webcam.py
+++ webcam.py
@@ -3,6 +3,7 @@
 import time
 import argparse
 import cv2
+import subprocess
 
 class Webcam:
     """Class to capture frame from webcam and save it to file."""
@@ -11,18 +12,24 @@ class Webcam:
         which camera to use."""
         self.camera_index = camera_index
 
+
+    def capset(self, cap, prop, value):
+        ret = cap.set(prop, value)
+        time.sleep(1)
+        print(f"Set {prop} to {value}, returned {ret}")
+
     def capture_frame(self):
         cap = cv2.VideoCapture(self.camera_index)
 
-        # set manual exposure level
-        # cap.set(cv2.CAP_PROP_EXPOSURE, -4)
+        # enable auto exposure -- doesn't work on all cameras
+        #self.capset(cap, cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
 
-        # enable auto exposure
-        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
+        # set manual exposure level - also doesn't work on all cameras
+        #self.capset(cap, cv2.CAP_PROP_EXPOSURE, -6)
 
         # set resolution
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        self.capset(cap, cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.capset(cap, cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
         ret, frame = cap.read()
         cap.release()
@@ -52,12 +59,15 @@ def main(time_interval=1, fname_prefix='frame'):
         fname = fname_prefix + time.strftime("%Y%m%d_%H%M%S") + ".jpg"
 
         webcam.save_frame(frame, fname)
+
+        subprocess.run(["open", fname])
+
         time.sleep(time_interval)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--time_interval", type=int,
-                        help="Time interval in seconds", default=1)
+                        help="Time interval in seconds", default=10)
     args = parser.parse_args()
 
     main(args.time_interval, "./image")
